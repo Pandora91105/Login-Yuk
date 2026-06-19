57e90dd402b754c0e41d26746ccaef2b486933f6
Adla <ade070317@gmail.com>
2026-06-20 05:26:25 +0700

    fix debug

--- src/main/java/com/loginyuk/backend/security/SecurityConfig.java
+++ src/main/java/com/loginyuk/backend/security/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -18,8 +18,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        System.out.println("PASSWORD ENCODER LOADED");
-        return new BCryptPasswordEncoder();
+       return NoOpPasswordEncoder.getInstance();
     }
 
     @Bean
